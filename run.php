106bcdfc9c405bfd23dbe7063623a909b74706ca
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2019-10-15 13:51:11 +0200

    Add BigImage slide

--- run.php
+++ run.php
@@ -15,6 +15,8 @@ $presentation = new Presentation\SlideShow(
     new Presentation\Template\Simple\FullscreenColor(Graphic\Color::white())
 );
 
+$drawer = new Adapter\Imagick\Graphic\Drawer();
+
 $presentation
     ->addSlide(new Slides\Mysterious())
     ->addSlide(new Slides\BigText("What do you\ndo with\nPHP?"))
@@ -22,22 +24,28 @@ $presentation
     ->addSlide(new Slides\BigText("API?"))
     ->addSlide(new Slides\BigText("CLI?"))
     ->addSlide(new Slides\Gif(
-        $bitmapSeqLoader->fromFile(__DIR__ . '/assets/boring.gif'))
+            $bitmapSeqLoader->fromFile(__DIR__.'/assets/boring.gif'))
     )
     ->addSlide(new Slides\BigText("What about\n…"))
     ->addSlide(new Slides\BigText("Music?", Graphic\Color::blue()))
     ->addSlide(new Slides\BigText("GUI?", Graphic\Color::red()))
     ->addSlide(new Slides\BigText("Video\nGames?", Graphic\Color::green()))
     ->addSlide(new Slides\Gif(
-        $bitmapSeqLoader->fromFile(__DIR__ . '/assets/confused.gif'))
+            $bitmapSeqLoader->fromFile(__DIR__.'/assets/confused.gif'))
     )
+    ->addSlide(new Slides\BigImage(
+    // Bug workaround: PNG has to be converted in BMP
+        $drawer->drawBitmap(
+            $bitmap = $bitmapLoader->fromFile(__DIR__.'/assets/elephpant.png'),
+            Geometry\Rect::fromSize($bitmap->size()),
+            Geometry\Rect::fromSize($bitmap->size())
+        )->toBitmap($bitmap->size())
+    ))
     ->addSlide(new Slides\Quote(
-        $bitmapLoader->fromFile(__DIR__.'/assets/rasmus.png'),
-        "There's an\nextension\nfor that!")
-    )
-;
+            $bitmapLoader->fromFile(__DIR__.'/assets/rasmus.png'),
+            "There's an\nextension\nfor that!")
+    );
 
 $screen = Presentation\Screen::fromSizeWithExpectedRatio(Geometry\Size::fromDimensions(800, 450));
 $engine = new Adapter\SDL\Render\Engine($screen);
-$drawer = new Graphic\Drawer\Cache(new Adapter\Imagick\Graphic\Drawer());
 $engine->start($presentation, $drawer);
---
+++ src/Slides/BigImage.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ForumPhp2019\Slides;
+
+use PhPresent\Geometry\Rect;
+use PhPresent\Graphic;
+use PhPresent\Presentation\Frame;
+use PhPresent\Presentation\Screen;
+use PhPresent\Presentation\Slide;
+use PhPresent\Presentation\Sprite;
+use PhPresent\Presentation\Timestamp;
+
+class BigImage implements Slide
+{
+    public function __construct(Graphic\Bitmap $bitmap)
+    {
+        $this->bitmap = $bitmap;
+    }
+
+    public function preload(Screen $screen, Graphic\Drawer $drawer, Graphic\Theme $theme): void
+    {
+        // Centering
+        $rect = Rect::fromSize($this->bitmap->size())
+            ->scaledBy(min(
+                $screen->fullArea()->size()->width() / $this->bitmap->size()->width(),
+                $screen->fullArea()->size()->height() / $this->bitmap->size()->height(),
+            ))
+            ->centeredOn($screen->safeArea()->center())
+        ;
+        $this->frame = new Frame(
+            Sprite::fromBitmap($this->bitmap)->moved($rect->topLeft())->resized($rect->size())
+        );
+    }
+
+    public function render(Timestamp $timestamp, Screen $screen, Graphic\Drawer $drawer, Graphic\Theme $theme)
+    {
+        return $this->frame;
+    }
+
+    /** @var Frame */
+    private $frame;
+    /** @var Graphic\Bitmap */
+    private $bitmap;
+}
